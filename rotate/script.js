aacbff99952ad689a7e9caf8112d854d2019315f
Chuug <chuug.pro@gmail.com>
2022-02-22 03:06:37 +0100

    rotation

--- rotate/script.js
+++ rotate/script.js
@@ -4,4 +4,5 @@ let deg = 0;
 setInterval(() => {
    deg += 45
    document.querySelector('.cube').style.transform = "rotate3d(1,2,1," + deg + "deg)";
+   // document.querySelector('.back').style.transform = `rotateY(${ deg }deg) translateZ(200px)`
 },1000);
