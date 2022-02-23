6dc35f22039558a30806b45049a93f275735f8f8
Chuug <chuug.pro@gmail.com>
2022-02-23 01:01:10 +0100

    animation learning

--- rotate/script.js
+++ rotate/script.js
@@ -3,6 +3,36 @@ let deg = 0;
 
 setInterval(() => {
    deg += 45
-   document.querySelector('.cube').style.transform = "rotate3d(1,2,1," + deg + "deg)";
+   // 
    // document.querySelector('.back').style.transform = `rotateY(${ deg }deg) translateZ(200px)`
 },1000);
+
+let cube = document.querySelector('.cube')
+let toggle = false
+
+cube.addEventListener('mouseover', () => {
+   cube.style.transform = "rotate3d(1,0,0,-90deg)"
+})
+
+cube.addEventListener('mouseout', () => {
+   cube.style.transform = "rotate3d(1,0,0,0deg)"
+   resetCube()
+})
+
+cube.addEventListener('click', () => {
+   cube.style.width = "400px"
+   cube.style.height = "300px"
+})
+
+const resetCube = () => {
+   cube.style.width = "200px"
+   cube.style.height = "50px"
+}
+
+function getPos(el) {
+   // yay readability
+   for (var lx=0, ly=0;
+        el != null;
+        lx += el.offsetLeft, ly += el.offsetTop, el = el.offsetParent);
+   return {x: lx,y: ly};
+}
